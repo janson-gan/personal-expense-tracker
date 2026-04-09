3ed6e22c95bd441883e8fd4b29b984568074666a
Janson-Gan <janson.gbc@gmail.comgit config --global user.name>
2026-04-09 15:59:25 +0800

    feat: Add error and not found middleware

---
+++ src/middlewares/error.middleware.ts
@@ -0,0 +1,23 @@
+import { Request, Response, NextFunction } from "express";
+import ApiError from "../errors/ApiError";
+
+export const errorHandler = (
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  if (err instanceof ApiError) {
+    res.status(err.statusCode).json({
+      success: false,
+      message: err.message,
+      statusCode: err.statusCode,
+    });
+    return;
+  }
+
+  res.status(500).json({
+    success: false,
+    message: "Internal server error",
+  });
+};
---
+++ src/middlewares/notFound.middleware.ts
@@ -0,0 +1,12 @@
+import { Request, Response, NextFunction } from "express";
+
+const notFoundHandler = (req: Request, res: Response, next: NextFunction) => {
+  res.status(404).json({
+    success: false,
+    message: "Not Found",
+  });
+
+  return;
+};
+
+export default notFoundHandler;
