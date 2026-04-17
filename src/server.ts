34f599d49496a8211afe9af007ceac4440cdb616
Janson-Gan <janson.gbc@gmail.comgit config --global user.name>
2026-04-17 10:55:13 +0800

    feat: Add DB connection

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
-import app from "./app";
-import config from "./config/config";
-import logger from "./utils/logger";
+import app from './app';
+import config from './config/config';
+import logger from './utils/logger';
+import { dbConnection } from './db/sequelize';
 
 app.listen(config.PORT, () => {
   logger.info(
     `Backend Server started listening at http://localhost:${config.PORT}`,
   );
 });
+
+dbConnection();
