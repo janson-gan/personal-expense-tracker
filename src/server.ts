3fabd68f58dfc697cf7efc0020af726124a0d24a
Janson-Gan <janson.gbc@gmail.comgit config --global user.name>
2026-04-24 15:12:20 +0800

    feat: Add ,env variables validation

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,24 @@ import app from './app';
 import config from './config/app.config';
 import logger from './utils/logger';
 import { dbConnection } from './db/sequelize';
+import validateEnv from './utils/validateEnv';
+
+// .env variables validation
+validateEnv([
+  'JWT_SECRET',
+  'DB_PASSWORD',
+  'DB_USER',
+  'DB_NAME',
+  'DB_HOST',
+  'DB_PORT',
+  'PORT',
+  'NODE_ENV',
+  'ORIGIN_URL'
+]);
 
 app.listen(config.PORT, () => {
   logger.info(
-    `Backend Server started listening at http://localhost:${config.PORT}`,
+    `Backend Server started listening at http://localhost:${process.env.PORT}`,
   );
 });
 
