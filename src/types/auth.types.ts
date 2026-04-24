bcfaae1d4d40709b468ea589bec1877a96887647
Janson-Gan <janson.gbc@gmail.comgit config --global user.name>
2026-04-24 15:13:37 +0800

    chore: Add login and jwt payload types

--- src/types/auth.types.ts
+++ src/types/auth.types.ts
@@ -1,6 +1,25 @@
+// Shape of data expected in register
 export interface RegisterType {
   name: string;
   email: string;
   password: string;
   role: 'student' | 'trainer' | 'admin';
+}
+
+// Shape of data expected in login
+export interface LoginType {
+  email: string;
+  password: string;
+}
+
+// Properties inside JWT
+export interface JWTPayloadType {
+  userId: string;
+  email: string;
+  role: 'student' | 'trainer' | 'admin';
+}
+
+// Extend Express request to include the decoded user JWT 
+export interface AuthenticatedRequest extends Express.Request {
+  user?: JWTPayloadType;
 }
