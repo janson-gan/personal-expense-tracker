bcd425c19e2cd9956b69fcb649d77ea2ca8429a4
Janson-Gan <janson.gbc@gmail.comgit config --global user.name>
2026-04-24 15:02:38 +0800

    chore: Add constant variables

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,4 +1,34 @@
-// Constant to replace hard coding (as const: read only)
+// Constant to replace hard coding (as const -> read only)
+
+// BCRYPT
 export const BCRYPT = {
-    SALT_ROUNDS: 12
-} as const
+  SALT_ROUNDS: 12,
+} as const;
+
+// JWT
+export const JWT = {
+  EXPIRES_DURATION: '1h',
+};
+
+// Request Headers
+export const AuthHeaders = {
+  BEARER: 'Bearer ',
+};
+
+// HTTP Status Code
+export const HTTP_STATUS = {
+  OK: 200,
+  CREATED: 201,
+  BAD_REQUEST: 400,
+  UNAUTHORISED: 401,
+  FORBIDDEN: 403,
+  NOT_FOUND: 404,
+  CONFLICT: 409,
+  INTERNAL_SERVER_ERROR: 500,
+} as const;
+
+export const ROLES = {
+  STUDENT: 'student',
+  TRAINER: 'trainer',
+  ADMIN: 'admin',
+} as const;
